d4e0135d98b73c3526050607a6e85928c94ec048
Lilyan Hatim Hassan <lilyanhatim@gmail.com>
2025-02-04 15:05:24 +0300

    Age calculator!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
+function Header() {
+
+  const currentYear = new Date().getFullYear();
+
+  const [age, setAge] = useState(0);
+  function handleAgeChange(e) {
+    setAge(e.target.value);
+  }
+
+  const [year, setYear] = useState(currentYear);
+  function handleYearChange(e) {
+    setYear(e.target.value);
+  }
+ 
+  var [futureAge, setFutureAge] = useState(0);
+  function calc() {
+    setFutureAge(Number(year) - Number(currentYear) + Number(age));
+  }
+  return (
+    <div className="header">
+      <label >How old are you?</label>
+      <input type="number" id="age" onChange={handleAgeChange} min="0" />
+      <label >In which year you wanna know your age?</label>
+      <input type="number" id="year" onChange={handleYearChange} />
+      <button onClick={calc} >Calculate</button>
+      <h3 >You are {age} years old, in {year} you will be {futureAge}</h3>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
